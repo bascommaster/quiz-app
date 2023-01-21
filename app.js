c71f08cd473c0b76e8c15c6f1e74355691f9334b
Mirosław Malik <mmiro390@gmail.com>
2023-01-21 15:26:25 +0100

    Added mongoose to project and connection to database.

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cookieSession = require("cookie-session");
 const config = require("./config");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+mongoose.connect(config.db, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("db connected");
+});
 
 const indexRouter = require("./routes/index");
 const newsRouter = require("./routes/news");
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  db: "mongodb+srv://miro:faraon@cluster0.axwjf8w.mongodb.net/?retryWrites=true&w=majority",
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000,
 };
